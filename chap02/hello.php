a80a87ce2a22fd9b0c6d2a303e664c351af5904b
ikasam <ikasam.k503@gmail.com>
2017-12-26 05:09:48 +0900

    Chapter2 hello world

---
+++ chap02/hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<title>PHPの基本</title>
+</head>
+<body>
+<?php
+// 'print' method is operation for display the string which is assigned.
+print 'Hello, World! <br />';
+print 'Hello, everyone!';
+?>
+</body>
+</html>
